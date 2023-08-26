1e0c6e9ae4f25981cedeab98834aec52fe0c8bf9
Mauro Lucas <30880379+MauroLucas@users.noreply.github.com>
2023-08-26 15:42:59 -0300

    Hello World

--- ClientRPC/Program.cs
+++ ClientRPC/Program.cs
@@ -5,9 +5,8 @@ using GrpcService1;
 var channel = GrpcChannel.ForAddress("http://localhost:50051");
 var client = new Greeter.GreeterClient(channel);
 
-var reply = client.SayHello(new HelloRequest { Name = "World" });
-var reply2 = client.SayHelloAgain(new HelloRequest { Name = "Mauro" });
+var reply = client.SayHello(new HelloRequest { Name = "Mauro" });
+
 Console.WriteLine("Greeting: " + reply.Message);
-Console.WriteLine("Greeting: " + reply2
-    .Message);
+
 
